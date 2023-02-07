c841083964bae4d10194791bb29dd1e4c3fadabe
R-set93 <roma.setetet@yandex.ru>
2023-02-07 23:12:35 +0300

    Для провеки

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,78 @@
+import java.util.Scanner;
+
 // dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        //System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        int countUser;
+        System.out.println("На скольких человек необходимо разделить счёт?");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                countUser = scanner.nextInt();
+                if (countUser == 1) {
+                    System.out.println("В этом случае нет смысла ничего считать и делить, введите корректное количество гостей");
+                } else if (countUser < 1) {
+                    System.out.println("Это некорректное значение для подсчёта, введите корректное количество гостей");
+                } else {
+                    nameProd.nameProde(countUser);
+                    break;
+                }
+            } else {
+                System.out.println("Это не число, введите корректное количество гостей");
+                scanner.next();
+            }
+        }
+    }
+
+}
+ class nameProd {
+    public static void nameProde(int countUser) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название товара");
+        double sum = 0;
+        String list = "Список товаров: \n";
+        String line = null;
+        String rubl = null;
+        double sale = 0;
+        while ((line = scanner.next()) != null) {
+            if (line.equalsIgnoreCase("Завершить")) {
+                System.out.println(list);
+                int num = (int) (sum / countUser);
+                if (num % 100 > 4 && num % 100 < 21) {
+                    rubl = "рублей";
+                } else if (num % 10 == 1) {
+                    rubl = "рубль";
+                } else if (num % 10 > 1 && num % 10 < 5) {
+                    rubl = "рубля";
+                }
+
+                String str2 = String.format("Каждый должен заплатить: %.2f" + " " + rubl, sum / countUser);
+                System.out.println(str2);
+                break;
+            }
+
+            System.out.println("Введите стоимость товара");
+            while (true) {
+                if (scanner.hasNextDouble()) {
+                    if (sale < 0) {
+                        System.out.println("Это некорректное значение для подсчёта");
+                    } else if (sale >= 0) {
+                        sale = scanner.nextDouble();
+                        list = list + line + "\n";
+                        sum = sum + sale;
+                        System.out.println("Товар " + line + " за " + sale + " успешно добавлен. Введите следующий товар");
+                        break;
+                    }
+                } else {
+                    System.out.println("Это не число, введите ещё");
+                    scanner.next();
+                }
+            }
+        }
     }
 }
+//System.out.println(String.format("%s,%d"a, b))
